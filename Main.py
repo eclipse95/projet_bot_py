e1e636d664b766bff8cfeb43a6fde48f2b76648e
Eclipse95 <alexis.bitaillou@univ-nantes.fr>
2014-12-17 12:00:52 +0100

    parser fini

--- Main.py
+++ Main.py
@@ -2,6 +2,4 @@ __author__ = 'E149248B'
 from class_plateau import *
 
 if __name__=='__main__':
-#    init_parser(str("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"))
-    parser_init_header(str("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"))
-    parser_init_node(str("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"))
+    parser_init(str("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"))
--- class_plateau.py
+++ class_plateau.py
@@ -3,20 +3,23 @@ from re import *
 
 
 class plateau():
-    def __init__(self, uid=0, ls_node=[], flag=0):
-        self.uid = uid  # UID du bot
-        self.flag = flag  # couleur joueur
-        self.liste_node = ls_node  # liste contenant les noeuds
-        # self.liste_joueur=[]
-        self.dico_aretes = dict()  #dico pour stocker les arretes
-
-    def add_node(self, node):  # methode pour ajouter un noeud (inutile normalement)
+    def __init__(self, uid=0,nb_player=2 ,nb_node=2, ls_node=[], flag=0,speed=1):
+        self.uid = uid                  # UID du bot
+        self.flag = flag                # couleur joueur
+        self.nb_node=nb_node            # nb de noeud
+        self.liste_node = ls_node       # liste contenant les noeuds
+        self.nb_player=nb_player        # nb de joueur
+        self.dico_aretes = dict()       #dico pour stocker les arretes
+        self.speed=speed                # vitesse
+    def add_node(self, node):           # methode pour ajouter un noeud (inutile normalement)
         self.liste_node.append(node)
 
     def define_dico(self, int_node1, int_node2):  # methode pour inserer les aretes
         self.dico_aretes = {}
         self.dico_aretes[int_node1] = int_node2
 
+    def update_uid(self,uid):                     # mise à jour uid
+        self.uid=uid
 
 def parser_init_header(chain):               #parser regex
     res=search('TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
@@ -49,3 +52,4 @@ def parser_init(chain):
         defsize=res.group(6+7*i)
         prod=res.group(7+7*i)
         ls_node.append(node(id,0,radius,[xpos,ypos],offsize,defsize,prod))
+    return nb_player,nb_node,ls_node,flag,speed
