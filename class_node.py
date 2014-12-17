ca2e870a21a6c8c7e6eecf199dff3664be18695a
Eclipse95 <alexis.bitaillou@univ-nantes.fr>
2014-12-17 10:46:41 +0100

    commentaire + parser

--- class_node.py
+++ class_node.py
@@ -1,17 +1,18 @@
 class node():
-    def __init__(self,owner=0,radius=None,pos=[],offsize=None,defsize=None,prod=1):
-        self.owner=owner
-        self.radius=radius
-        self.pos=pos
-        self.offsize=offsize
-        self.defsize=defsize
-        self.prod=prod
-        self.neighbor=[]
+    def __init__(self,owner=0,radius=None,pos=[],offsize=None,defsize=None,prod_def=1,prod_off=1):
+        self.owner=owner                #n° du joueur
+        self.radius=radius              #rayon du noeud
+        self.pos=pos                    #position
+        self.offsize=offsize            #unité off
+        self.defsize=defsize            #unité def
+        self.prod_def=prod_def          #
+        self.prod_off=prod_off
+        self.neighbor=[]                #liste des noueds voisins
 
     def update(self,owner,offsize,defsize):         #mise à jour du noeud
-        self.owner=owner
-        self.offsize=offsize
-        self.defsize=defsize
+        self.owner=owner                #n° joueur
+        self.offsize=offsize            #unité off
+        self.defsize=defsize            #unité def
 
     def addneighbor(self,int_value):                #ajoute le numéro des voisins aux noeuds
         self.neighbor.append(int_value)
--- class_plateau.py
+++ class_plateau.py
@@ -1,36 +1,42 @@
 from class_node import *
+from re import *
+
 
 class plateau():
-    def __init__(self,uid=0,ls_node=[],flag=0):
-        self.uid=uid                        #UID du bot
-        self.flag=flag                      #couleur joueur
-        self.liste_node=ls_node             #liste contenant les noeuds
-        #self.liste_joueur=[]
-        self.dico_aretes=dict()             #dico pour stocker les arretes
+    def __init__(self, uid=0, ls_node=[], flag=0):
+        self.uid = uid  # UID du bot
+        self.flag = flag  # couleur joueur
+        self.liste_node = ls_node  # liste contenant les noeuds
+        # self.liste_joueur=[]
+        self.dico_aretes = dict()  #dico pour stocker les arretes
 
-    def add_node(self,node):                #methode pour ajouter un noeud (inutile normalement)
+    def add_node(self, node):  # methode pour ajouter un noeud (inutile normalement)
         self.liste_node.append(node)
 
-    def define_dico(self,int_node1,int_node2):                  #methode pour inserer les aretes
-        self.dico_aretes={}
-        self.dico_aretes[int_node1]=int_node2
+    def define_dico(self, int_node1, int_node2):  # methode pour inserer les aretes
+        self.dico_aretes = {}
+        self.dico_aretes[int_node1] = int_node2
 
-def  init_parser(chain):
+
+def init_parser(chain):  # parser pas propre
     #"INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"
-    k=1                     #compteur pour la chain
-    while(str(chain[k-1])!=str('T') or str(chain[k])!=str('O')):           #avance jusqu'à la chaine 'TO'
-        k+=1
-    k+=1
-    nb_player=''
-    while(str(chain[k])!=str('[')):                  #avance jusqu'au nombre de joueur
-        nb_player+=chain[k]
-        k+=1
-    k+=1
-    flag=''
-    while(str(chain[k]!=str(']'))):
-        flag+=chain[k]
-        k+=1
+    k = 1  #compteur pour la chain
+    while (str(chain[k - 1]) != str('T') or str(chain[k]) != str('O')):  #avance jusqu'à la chaine 'TO'
+        k += 1
+    k += 1
+    nb_player = ''
+    while (str(chain[k]) != str('[')):  #avance jusqu'au nombre de joueur
+        nb_player += chain[k]
+        k += 1
+    k += 1
+    flag = ''
+    while (str(chain[k] != str(']'))):
+        flag += chain[k]
+        k += 1
 
     print(flag)
     print(nb_player)
-    
+
+
+def parser_init_clean(chain):
+    search('TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
