5c83275fcb5e8acda04ad58e974dc7ebab2618c7
eclipse95 <alexis.bitaillou@etu.univ-nantes.fr>
2014-12-27 20:08:46 +0100

    add server/client function to main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,16 @@
 __author__ = 'E149248B'
 from class_plateau import *
+import inspect
+import logging
+
 
 if __name__=='__main__':
     tmp=parser_init(str("INIT20ac18ab-6d18-450e-94af-bee53fdc8fcaTO6[2];1;3CELLS:1(23,9)'2'30'8'I,2(41,55)'1'30'8'II,3(23,103)'1'20'5'I;2LINES:1@3433OF2,1@6502OF3"))
+
+def register_pooo(uid):                 #je ne sais pas comment ça marche mais ça marche
+    pass
+def init_pooo(init_string):
+    pass
+def play_pooo():
+    logging.info('Entering play_pooo fonction from {} module...'.format(inspect.currentframe().f_back.f_code.co_filename))
+    pass
