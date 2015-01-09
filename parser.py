a0ebd2bf0b981d3d37ed2b804da34e60fb6cae4a
eclipse95 <alexis.bitaillou@etu.univ-nantes.fr>
2015-01-09 12:37:33 +0100

    parser upgrade

--- parser.py
+++ parser.py
@@ -15,11 +15,12 @@ def parser_init_node(chain):
     print(res)
 
 def parser_init(chain):
-    res=search('TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
-    nb_player=int(res.group(1))
-    flag=int(res.group(2))
-    speed=int(res.group(3))
-    nb_node=int(res.group(4))
+    res=search('INIT(.+)TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
+    matchid=str(res.group(1))
+    nb_player=int(res.group(2))
+    flag=int(res.group(3))
+    speed=int(res.group(4))
+    nb_node=int(res.group(5))
     res1=findall("(\d*?)\((\d*?)\,(\d*?)\)'(\d*?)'(\d*?)'(\d)'(\w*)",chain)
     nb_aretes=int(search(";(\d)LINES:",chain).group(0))
     res=findall("(\d)@(\d+)OF(\d)",chain)
@@ -37,7 +38,8 @@ def parser_init(chain):
             if(id==res[j][0]):
                 ls_aretes.append(res[j][2],res[j][1])
         ls_node.append(node(id,0,radius,[xpos,ypos],offsize,defsize,prod,ls_aretes))
-    return nb_player, nb_node, ls_node, flag, speed         #problème de retour
+    return matchid, nb_player, nb_node, ls_node, flag, speed         #problème de retour
 
 def parser_state(chain):
-    
+    #STATE20ac18ab-6d18-450e-94af-bee53fdc8fcaIS2;3CELLS:1[2]12'4,2[2]15'2,3[1]33'6;4MOVES:1<5[2]@232'>6[2]@488'>3[1]@4330'2,1<10[1]@2241'3
+    pass
