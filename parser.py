5d40d25ee13927769e65c24a8b40b6e17ed42483
eclipse95 <alexis.bitaillou@etu.univ-nantes.fr>
2015-01-11 21:08:09 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	class_plateau.py

--- parser.py
+++ parser.py
@@ -1,7 +1,7 @@
 from re import *                            # lib regex
 from class_node import *
 
-def parser_init_header(chain):               #parser regex
+def parser_init_header(chain):               #parser regex (obsolète)
     res=search('TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
     nb_player=res.group(1)
     flag=res.group(2)
@@ -10,22 +10,22 @@ def parser_init_header(chain):               #parser regex
     print(nb_player,flag,speed,nb_node)
 
 
-def parser_init_node(chain):
+def parser_init_node(chain):                #parser obsolète
     res=findall("(\d*?)\((\d*?)\,(\d*?)\)'(\d*?)'(\d*?)'(\d)'(\w*)",chain)
     print(res)
 
-def parser_init(chain):
-    res=search('INIT(.+)TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)
+def parser_init(chain):                     # parser chaine init
+    res=search('INIT(.+)TO(\d)\[(\d)\];(\d);(\d)CELLS:',chain)  #parse parametres match
     matchid=str(res.group(1))
     nb_player=int(res.group(2))
     flag=int(res.group(3))
     speed=int(res.group(4))
     nb_node=int(res.group(5))
-    res1=findall("(\d*?)\((\d*?)\,(\d*?)\)'(\d*?)'(\d*?)'(\d)'(\w*)",chain)
+    res1=findall("(\d*?)\((\d*?)\,(\d*?)\)'(\d*?)'(\d*?)'(\d)'(\w*)",chain)     #parse les noeuds
     nb_aretes=int(search(";(\d)LINES:",chain).group(0))
-    res=findall("(\d)@(\d+)OF(\d)",chain)
+    res=findall("(\d)@(\d+)OF(\d)",chain)           #parse les aretes
     ls_node=[]
-    for i in range (nb_node):
+    for i in range (nb_node):                       #assemblage des noeuds
         id=int(res1[i][0])
         xpos=int(res1[i][1])
         ypos=int(res1[i][2])
@@ -37,8 +37,8 @@ def parser_init(chain):
         for j in range(nb_aretes):
             if(id==res[j][0]):
                 ls_aretes.append(res[j][2],res[j][1])
-        ls_node.append(node(id,0,radius,[xpos,ypos],offsize,defsize,prod,ls_aretes))
-    return matchid, nb_player, nb_node, ls_node, flag, speed         #problème de retour
+        ls_node.append(node(id,0,radius,[xpos,ypos],offsize,defsize,prod,ls_aretes))    #liste de noeud
+    return matchid, nb_player, nb_node, ls_node, flag, speed         #retourne une liste
 
 def lire_state(string):
     regex = re.compile('STATE.+;\dCELLS')
@@ -67,6 +67,6 @@ def lire_state(string):
 
 def parser_state(chain,board):
     #STATE20ac18ab-6d18-450e-94af-bee53fdc8fcaIS2;3CELLS:1[2]12'4,2[2]15'2,3[1]33'6;4MOVES:1<5[2]@232'>6[2]@488'>3[1]@4330'2,1<10[1]@2241'3
-    if(str(search('STATE(.+)IS\d;\dCELLS',chain).group(0))==str(board.matchid)):
-        res1=findall("(\d+)\[(\d+)\](\d)'(\d)",chain)
+    if(str(search('STATE(.+)IS\d;\dCELLS',chain).group(0))==str(board.matchid)):    #verifie le match ID
+        cells=findall("(\d+)\[(\d+)\](\d)'(\d)",chain)
 
